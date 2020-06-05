b7359a37193a557e0cf0781b53b116738bcf42da
Justin Huang <justinhuang777@gmail.com>
2020-06-05 17:45:37 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -39,7 +39,11 @@ router.post('/register', (req, res) => {
         .catch(err => console.log(err))
     }
   })
+})
 
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
 })
 
 module.exports = router
